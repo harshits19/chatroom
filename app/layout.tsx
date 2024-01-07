981c89e3c2f09751360a2c1332705563f62a4b9c
Harshit <gaurharshit4@gmail.com>
2024-01-08 00:33:30 +0530

    initial app

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: "Chatroom",
+  description: "Your personalized rooms for chatting with close friends.",
+  icons: {
+    icon: "/ico1.png",
+  },
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
